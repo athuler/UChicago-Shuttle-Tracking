21053f33ede6ae286558409a41baa8c32f2f0b26
athuler <22741115+athuler@users.noreply.github.com>
2024-04-18 10:59:44 -0500

    MySQL DB Connection Test

--- dbTest.py
+++ dbTest.py
@@ -1,10 +1,11 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from secrets import *
+import mysql.connector
 
-
-# Create a new client and connect to the server
-client = MongoClient(DB_URL, server_api=ServerApi('1'))
+"""
+# Create a new Mongo DB client and connect to the server
+client = MongoClient(MONGO_DB_URL, server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
 try:
@@ -12,3 +13,18 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+"""
+
+# Check local DB
+
+try:
+	cnx = mysql.connector.connect(
+		user = DB_USER,
+		password = DB_PASS,
+		host = DB_HOST,
+		database = DB_NAME
+	)
+	cnx.close()
+	print("Successfully connected")
+except Exception as e:
+	print(e)
