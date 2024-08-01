43c74d5767fb526832043e04dacf8f78411fc962
athuler <22741115+athuler@users.noreply.github.com>
2024-08-01 16:42:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
 	version=__version__,
 	description="Tracking the Performance of UChicago's Shuttle Network",
 	long_description=long_description,
+	long_description_content_type='text/markdown',
 	author='Andrei Thuler',
 	author_email='info@andreithuler.com',
 	url="https://github.com/athuler/UChicago-Shuttle-Tracking",
